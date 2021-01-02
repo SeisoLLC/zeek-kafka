e5e6990f8b2c46be009055de8d7ab8e9c74a2667
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2021-01-01 21:25:54 -0500

    Add copyright to modified files

--- docker/in_docker_scripts/build_plugin.sh
+++ docker/in_docker_scripts/build_plugin.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 #
+#  Copyright 2020-2021 Zeek-Kafka Contributors
+#
 #  Licensed to the Apache Software Foundation (ASF) under one or more
 #  contributor license agreements.  See the NOTICE file distributed with
 #  this work for additional information regarding copyright ownership.
