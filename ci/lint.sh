f4c5160eb97a2dccef7b4071d7b8c4684b0e7dd9
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2021-01-06 08:24:17 -0500

    Fix linting

--- ci/lint.sh
+++ ci/lint.sh
@@ -9,29 +9,25 @@ set -E # errtrap
 set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
-FILES=$(find "${DIR}" \( -path "${DIR}/.git" -prune -or       \
-                         -path "${DIR}/.github" -prune \) -or \
-              \(      -not -name ".*"                         \
-                 -and -not -name "*.yml"                      \
-                 -and -not -name "*.pcap"                     \
-                 -and -not -name "*.pcapng"                   \
-                 -and -not -name "requirements*.txt"          \
-                 -and -not -name "Dockerfile"                 \
-                 -and -not -name "output"                     \
-                 -and -not -name "random.seed"                \
-                 -and -not -name "COPYING"                    \
-                 -and -not -name "zkg.meta"                   \
-                      -type f                                 \
-              \)                                              \
-       )
+MISSING_COPYRIGHT=$(find "${DIR}" \( -path "${DIR}/.git" -or                     \
+                 -path "${DIR}/.github" \) -prune -false -or \
+         \(      -not -name ".*"                             \
+            -and -not -name "*.yml"                          \
+            -and -not -name "*.pcap"                         \
+            -and -not -name "*.pcapng"                       \
+            -and -not -name "requirements*.txt"              \
+            -and -not -name "Dockerfile"                     \
+            -and -not -name "output"                         \
+            -and -not -name "random.seed"                    \
+            -and -not -name "COPYING"                        \
+            -and -not -name "zkg.meta"                       \
+                 -type f                                     \
+         \)                                                  \
+                 -print0 |                                   \
+  xargs -0 grep --files-without-match 'Copyright 2020-2.* Zeek-Kafka$')
 
-while IFS= read -r file; do
-  # If the file still exists, and matches one of the provided extensions
-  if [[ -f "${file}" ]]; then
-    # Ensure that it has the required copyright statemenet
-    grep --files-without-match 'Copyright 2020-2.* Zeek-Kafka$' "${file}"
-  else
-    echo "${file} was excluded from linting"
-  fi
-done <<< "${FILES}"
-
+if [[ "${MISSING_COPYRIGHT}" ]]; then
+  echo "The following files are missing the Zeek-Kafka copyright"
+  echo "* ${MISSING_COPYRIGHT}"
+  exit 1
+fi
