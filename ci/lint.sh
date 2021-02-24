31cef29979a951faecfcfe61efb110a347fa7f10
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2021-02-24 13:00:49 -0500

    M

--- ci/lint.sh
+++ ci/lint.sh
@@ -35,8 +35,9 @@ MISSING_COPYRIGHT=$(find "${DIR}" \( -path "${DIR}/.git" -or                    
             -and -not -name "zkg.meta"                       \
                  -type f                                     \
          \)                                                  \
-                 -print0 |                                   \
-  xargs -0 grep --files-without-match 'Copyright 2020-2.* Zeek-Kafka$')
+	 -print0 |                                           \
+  xargs -0 grep --files-without-match 'Copyright 2020-2.* Zeek-Kafka$' || true)
+echo missing
 
 if [[ "${MISSING_COPYRIGHT:-}" ]]; then
   echo "The following files are missing the Zeek-Kafka copyright"
