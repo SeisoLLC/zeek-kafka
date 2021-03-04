0798e97ff601b303a053a1e7417e32c6d9a58e4d
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2021-03-04 14:53:50 -0500

    hardcode main

--- docker/in_docker_scripts/build_plugin.sh
+++ docker/in_docker_scripts/build_plugin.sh
@@ -96,14 +96,14 @@ cd /root || exit 1
 
 echo "==================================================="
 
-zkg -vvv test zeek/seisollc/zeek-kafka --version "${PLUGIN_VERSION}"
+zkg -vvv test zeek/seisollc/zeek-kafka --version "main"
 rc=$?; if [[ ${rc} != 0 ]]; then
   echo "ERROR running zkg test ${rc}"
   printfiles
   exit ${rc}
 fi
 
-zkg -vvv install zeek/seisollc/zeek-kafka --skiptests --version "${PLUGIN_VERSION}" --force
+zkg -vvv install zeek/seisollc/zeek-kafka --skiptests --version "main" --force
 rc=$?; if [[ ${rc} != 0 ]]; then
   echo "ERROR running zkg install ${rc}"
   printfiles
