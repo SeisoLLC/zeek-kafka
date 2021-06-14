e1f938b71bba7591667d212b0fe6e91ba57297ba
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2021-06-14 18:54:02 -0400

    Fix headers

--- tasks.py
+++ tasks.py
@@ -5,7 +5,6 @@ Task execution tool & library
 
 #
 #  Copyright 2020-2021 Zeek-Kafka
-#  Copyright 2015-2020 The Apache Software Foundation
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
